14da922121ec2842f6414f8a4e01818e63c7de4b
Neoon <neo@tiefkuehler-madness.me>
2021-06-27 20:33:36 +0200

    add / list / del machine

--- Class/cli.py
+++ Class/cli.py
@@ -3,7 +3,15 @@ import json, time
 
 class CLI(rqlite):
 
-    def addContainer(self,data):
+    def addMachine(self,data):
         print("deploying",data[0])
         response = self.execute(['INSERT INTO machines(name,os,memory,deploy) VALUES(?, ?, ?, ?)',data[0],data[1],data[2],data[3]])
+        print(json.dumps(response, indent=4, sort_keys=True))
+
+    def getTable(self,table="machines"):
+        response = self.query(["SELECT rowid, * FROM "+table])
+        print(json.dumps(response, indent=4, sort_keys=True))
+
+    def deleteMachine(self,data):
+        response = self.execute(['DELETE FROM machines WHERE name=?',data[0]])
         print(json.dumps(response, indent=4, sort_keys=True))
--- cli.py
+++ cli.py
@@ -5,6 +5,15 @@ import sys
 cli = CLI()
 
 if len(sys.argv) == 1:
-    print("init")
+    print("init, machine")
 elif sys.argv[1] == "init":
     cli.init()
+elif sys.argv[1] == "machine":
+    if len(sys.argv) == 2:
+        print("machine add <name> <os> <memor> <deploy>\machine list\nmachine del <name>")
+    elif sys.argv[2] == "add":
+        cli.addMachine(sys.argv[3:])
+    elif sys.argv[2] == "list":
+        cli.getTable("machines")
+    elif sys.argv[2] == "del":
+        cli.deleteMachine(sys.argv[3:])
