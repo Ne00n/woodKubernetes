e85f1d9f69ec6f3527dd70f48176ee5295f68ade
Neoon <neo@tiefkuehler-madness.me>
2021-10-13 15:20:38 +0200

    move cert to cert.py cron

---
+++ cron/cert.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+
+from Class.cert import Cert
+
+cert = Cert()
+
+while True:
+    cert.renew())
+    time.sleep(30)
--- cron/wood.py
+++ cron/wood.py
@@ -3,13 +3,9 @@ import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 
 from Class.lxd import LXD
-from Class.cert import Cert
 
 if len(sys.argv) == 1:
-    print("lxd, cert")
+    print("lxd")
 elif sys.argv[1] == "lxd":
     lxd = LXD()
     lxd.run()
-elif sys.argv[1] == "cert":
-    cert = Cert()
-    cert.renew()
