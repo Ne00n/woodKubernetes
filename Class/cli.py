224063418bfdc77262f55b2963f1ac9938798079
Neoon <neo@tiefkuehler-madness.me>
2022-06-01 10:30:13 +0200

    update cli

--- Class/cli.py
+++ Class/cli.py
@@ -4,7 +4,7 @@ import json, time
 class CLI(rqlite):
 
     def addMachine(self,data):
-        print("deploying",data[0])
+        print(f"Deploying {data[0]}")
         mount = data[5] if len(data) > 5 else "none"
         ports = data[4] if len(data) > 4 else 0
         response = self.execute(['INSERT INTO machines(name,os,memory,deploy,ports,mount) VALUES(?, ?, ?, ?, ?, ?)',data[0],data[1],data[2],data[3],ports,mount])
