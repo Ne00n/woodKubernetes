1dfec6d4a7fad21a86f20886a4867e6491986cca
Neoon <neo@tiefkuehler-madness.me>
2022-06-05 15:33:01 +0200

    update lxd for replica

--- Class/lxd.py
+++ Class/lxd.py
@@ -47,18 +47,18 @@ class LXD(rqlite):
 
             machineList,containerList = {},[]
             for machine in self.table(machines):
-                machineList[machine['name']] = {"nodes":machine['nodes'],"memory":machine['memory']}
+                machineList[machine['name']] = machine
 
             current = nodes[hostname]
             if current['leader'] is True:
                 for machine,details in machineList.items():
                     #check if anything is not allocated
-                    if details['node'] is None:
-                        self.switchMachine(nodes,machine,machines,hostMemory,details['memory'])
+                    if details['nodes'] is None:
+                        self.switchMachine(nodes,hostname,machine,machines,hostMemory,details)
                         continue
                     #checking if anything wen't down
-                    if details['node'] in nodes and nodes[details['node']]['reachable'] is not True:
-                        self.switchMachine(nodes,machine,machines,hostMemory,details['memory'])
+                    if set(details['nodes'].split(',')).issubset(nodes) and nodes[details['node']]['reachable'] is not True:
+                        self.switchMachine(nodes,hostname,machine,machines,hostMemory,details,True)
 
             #check existing containers
             for container in containers:
@@ -67,14 +67,14 @@ class LXD(rqlite):
                     self.terminate(container)
             #check if we should deploy a new one
             for machine in self.table(machines):
-                if machine['nodes'] == hostname and machine['name'] not in containerList:
+                if hostname in machine['nodes'].split(",") and machine['name'] not in containerList:
                     self.deploy(machine)
             time.sleep(10)
 
     def getMemoryUsage(self,node,machines):
         total = 0
         for machine in self.table(machines):
-            if machine['nodes'] == node: total = total + int(machine['memory'])
+            if node in machine['nodes'].split(","): total = total + int(machine['memory'])
         return total
 
     def deploy(self,machine):
@@ -96,14 +96,14 @@ class LXD(rqlite):
             ports = machine['ports'].split(",")
             for port in ports:
                 ingress, egress = port.split(':')
-                subprocess.call(['lxc','config','device','add',machine[0],f'port{str(ingress)}','proxy',f'listen=tcp:0.0.0.0:{str(egress)}',f'connect=tcp:127.0.0.1:{str(egress)}'])
+                subprocess.call(['lxc','config','device','add',machine['name'],f'port{str(ingress)}','proxy',f'listen=tcp:0.0.0.0:{str(egress)}',f'connect=tcp:127.0.0.1:{str(egress)}'])
         if machine['mounts'] != "none":
             print(f"{machine['name']} Mounts")
             mounts = machine['mounts'].split(",")
             for mount in mounts:
                 parts = mount.split(":")
                 source, path = mount.split(':')
-                subprocess.call(['lxc','config','device','add',machine[0],{source},'disk',f'source={source}',f'path={path}'])
+                subprocess.call(['lxc','config','device','add',machine['name'],{source},'disk',f'source={source}',f'path={path}'])
 
     def terminate(self,machine):
         print("Deleting",machine['name'])
@@ -111,14 +111,21 @@ class LXD(rqlite):
             subprocess.call(['lxc', 'stop',machine['name']])
         subprocess.call(['lxc', 'delete',machine['name']])
 
-    def switchMachine(self,nodes,machine,machines,hostMemory,memory):
+    def switchMachine(self,nodes,hostname,machine,machines,hostMemory,details,outage=False):
         #randomize
         nodeItems = list(nodes.items())
         random.shuffle(nodeItems)
         nodes = dict(nodeItems)
+        #switching
         print("Switching",machine)
+        deploy = details['nodes'].split(',') if outage: else []
         for node,data in nodes.items():
-            if data['reachable'] is True and hostMemory > int(memory) + self.getMemoryUsage(node,machines):
+            if data['reachable'] is True and hostMemory > int(details['memory']) + self.getMemoryUsage(node,machines) and node not in deploy:
                 print("Switching",machine,"to",node)
-                self.execute(['UPDATE machines SET node = ? WHERE name = ?',node,machine])
-                break
+                #remove old node that has been marked as offline
+                if outage and hostname in deploy: deploy.remove(hostname)
+                #append new node
+                deploy.append(node)
+                #break if replica limit reached or 0 for all nodes
+                if details['replica'] != 0 and details['replica'] == len(deploy): break
+        self.execute(['UPDATE machines SET node = ? WHERE name = ?',','.join(deploy),machine])
